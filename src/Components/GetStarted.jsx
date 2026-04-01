88a61e449b3840ebb0c69f0d4f0e060ffcf491dd
rashedpine83 <rashedpine83@gmail.com>
2026-04-01 22:54:02 +0600

    Modify get started component

--- src/Components/GetStarted.jsx
+++ src/Components/GetStarted.jsx
@@ -16,26 +16,31 @@ const GetStarted = () => {
                     <div className='flex justify-end'>
                         <span className='bg-violet-600 px-2 rounded-full text-white font-bold'>1</span>
                     </div>
+
                     <div className='flex justify-center'>
                         <a href=""> <img className='bg-gray-200 rounded-full p-2' src={user} alt="user" /></a>
-                       
                     </div>
+
                     <div className='text-center'>
                         <a href=""><h3 className='text-2xl font-bold mb-5'>Create Account</h3></a>
                         <p className='text-[#627382]'>Sign up for free in seconds. No credit card required to get started.</p>
                     </div>
                 </div>
+
                 <div className='shadow-[0_4px_20px_rgba(0,0,0,0.25)] p-10 space-y-5 rounded-lg'>
                     <div className='flex justify-end'>
                         <span className='bg-violet-600 px-2 rounded-full text-white font-bold'>2</span>
                     </div>
+
                     <div className='flex justify-center'>
                         <a href=""> <img className='bg-gray-200 rounded-full p-2' src={packageimg} alt="user" /></a>
                     </div>
+
                     <div className='text-center'>
                         <a href=""><h3 className='text-2xl font-bold mb-5'>Choose Products</h3></a>
                         <p className='text-[#627382]'>Browse our catalog and select the tools that fit your needs.</p>
                     </div>
+                    
                 </div>
                 <div className='shadow-[0_4px_20px_rgba(0,0,0,0.25)] p-10 space-y-5 rounded-lg'>
                     <div className='flex justify-end'>
